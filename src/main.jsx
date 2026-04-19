e206c48428539cd52015f9525d718bcfa0bc723d
Ashton Vernon <amvernon@wisc.edu>
2026-04-19 15:06:14 -0500

    Hash Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/content/Layout';
 import HomePage from './components/content/HomePage';
 import MyTrips from './components/content/MyTrips';
@@ -14,7 +14,7 @@ import './css/HomePage.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/p55/">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
@@ -27,6 +27,6 @@ root.render(
         {/* Redirect any unknown route to home */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
